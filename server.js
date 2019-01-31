963d6abba8811a72fe0b599e34953d0b2a6401f7
safwaan chowdhury <safwaan@timesaved.ca>
2019-01-31 11:39:44 -0500

    Added comments and removed maintenance.hbs (not needed

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
+//logs the time, request method and the path when an http request is made
+//only paramter is callback; middleware executed for every request
 app.use((req, res, next) => {
     var now = new Date().toString();
     var log = `${now}: ${req.method} ${req.url}`;
@@ -20,22 +22,19 @@ app.use((req, res, next) => {
     next();
 });
 
-//app.use((req, res, next) => {
-//    res.render('maintenance.hbs');
-//});
-
 app.use(express.static(__dirname + '/public'));
 
+//html templates
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
 });
 
 hbs.registerHelper('screamIt', (text) => {
     return text.toUpperCase();
-})
+});
 
-app.get('/', (req, res) => {
-    //res.send('<h1>Hello Express<h1>');
+//middleware routes to render different pages
+app.get('/', (req, res,) => {
     res.render('home.hbs', {
         pageTitle: 'Home',
         welcomeMessage: 'Hello There'
@@ -50,11 +49,11 @@ app.get('/about', (req, res) => {
 
 app.get('/bad', (req, res) => {
     res.send ({
-        errorMessage: 'Error'   
+        errorMessage: 'Error has occured'   
     });
 });
 
-
+//starts a UNIX socket and listens for connections on the given path
 app.listen(3000, () => {
     console.log('Server is up on port 3000');
 });
